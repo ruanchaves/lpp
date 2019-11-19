ad88b040956b0258334a694cbdbda0af70d363fd
Ruan Chaves <ruanchaves93@gmail.com>
2019-11-19 13:53:07 -0400

    Update schemeexamples.scm

--- schemeexamples.scm
+++ schemeexamples.scm
@@ -2,15 +2,14 @@
 (area 2 4 6)
 
 (define delta (lambda (b a c) (- (* b b  ) (* 4 a c))))
-
-(delta 2 4 6)
-
 (define raiz (lambda (b a c op)  (* (op (- b) (sqrt (delta b a c)))) (/ 1 (* 2 a)) ))
-
 (define raizPos (lambda (b a c) (raiz b a c +) ))
-
 (define raizNeg (lambda (b a c) (raiz b a c -) ))
-
 (raizPos 4 1 4)
 (raizNeg 4 1 4)
 (delta 4 1 4)
+
+(define nota (lambda (x) (if (< x  4) "Reprovado" (if (< x  6) "Reforço" "Aprovado"))))
+(nota 3)
+(nota 5)
+(nota 7)
